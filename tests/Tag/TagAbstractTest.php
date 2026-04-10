f1a21a935b1510f23949532201ce901466aa57d1
Gemorroj <wapinet@mail.ru>
2026-04-10 18:28:03 +0300

    build: upd deps

--- tests/Tag/TagAbstractTest.php
+++ tests/Tag/TagAbstractTest.php
@@ -3,6 +3,7 @@
 namespace Xbbcode\Tests\Tag;
 
 use PHPUnit\Framework\TestCase;
+use Xbbcode\Attributes;
 use Xbbcode\Tag\TagAbstract;
 
 class TagAbstractTest extends TestCase
@@ -12,7 +13,18 @@ class TagAbstractTest extends TestCase
         $validTargets = ['_blank', '_self', '_parent', '_top', 'any_anchor', '123'];
         $invalidTargets = ['_fake', '_123'];
 
-        $mock = $this->getMockBuilder(TagAbstract::class)->getMock();
+        $mock = new class extends TagAbstract {
+            public function __toString(): string
+            {
+                throw new \RuntimeException('Not implemented');
+            }
+
+            protected function getAttributes(): Attributes
+            {
+                throw new \RuntimeException('Not implemented');
+            }
+        };
+
         $method = new \ReflectionMethod($mock, 'isValidTarget');
 
         foreach ($validTargets as $target) {
